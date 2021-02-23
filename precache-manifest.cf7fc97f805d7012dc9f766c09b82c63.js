837ef002675e750500cad91654360017914dd34e
Yasir <yaseer@nanosoftengineers.com>
2021-02-23 11:45:19 +0530

    Source

---
+++ precache-manifest.cf7fc97f805d7012dc9f766c09b82c63.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aeefe871c06fb9ec28a7092d5d2a0a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c27dff5857369ba39d0",
+    "url": "/static/css/2.72c4868e.chunk.css"
+  },
+  {
+    "revision": "4c7f6d0462690c96426b",
+    "url": "/static/css/main.e4605ca0.chunk.css"
+  },
+  {
+    "revision": "5c27dff5857369ba39d0",
+    "url": "/static/js/2.34ddbfd9.chunk.js"
+  },
+  {
+    "revision": "8a855a0c8042ae048e13afe520fc16bd",
+    "url": "/static/js/2.34ddbfd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7f6d0462690c96426b",
+    "url": "/static/js/main.819e88e4.chunk.js"
+  },
+  {
+    "revision": "0163baa1ef229f912f7af122d7843428",
+    "url": "/static/js/main.819e88e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "930efa2c67e7f932bcaf",
+    "url": "/static/js/runtime-main.96b5ce5c.js"
+  },
+  {
+    "revision": "20ed567d8809ba8e93212d571ba50335",
+    "url": "/static/media/BuildingLoader.20ed567d.gif"
+  },
+  {
+    "revision": "3db037116b18617db19c8bfc89398859",
+    "url": "/static/media/NoData.3db03711.gif"
+  },
+  {
+    "revision": "e3ce3b962813bdb0821ef1d1b69f3ebd",
+    "url": "/static/media/searching.e3ce3b96.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
